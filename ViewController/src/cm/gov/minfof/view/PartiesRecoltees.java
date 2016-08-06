33b262d38be7d0674f1e67532e9c13b989de0042
abbo <tblaiso@gmail.com>
2016-08-06 09:26:15 +0100

    test

---
+++ ViewController/src/cm/gov/minfof/view/PartiesRecoltees.java
@@ -0,0 +1,49 @@
+package cm.gov.minfof.view;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class PartiesRecoltees {
+    public PartiesRecoltees() {
+    }
+
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+    
+    public BindingContainer getBindingContainer() {
+
+    BindingContext bindingContext = BindingContext.getCurrent();
+    return bindingContext.getCurrentBindingsEntry();
+    }
+    
+    public Object executemethode(String mthod){
+        BindingContainer bds = getBindingContainer();
+        OperationBinding ob = bds.getOperationBinding(mthod);
+        
+        return ob.execute();
+    }
+
+    public String enregPartiesRecoltees() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String supprimePartieRecoltees() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Delete");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+}
---
+++ ViewController/src/cm/gov/minfof/view/ProduitPfnl.java
@@ -0,0 +1,60 @@
+package cm.gov.minfof.view;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class ProduitPfnl {
+    public ProduitPfnl() {
+    }
+
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+    
+    public BindingContainer getBindingContainer() {
+
+    BindingContext bindingContext = BindingContext.getCurrent();
+    return bindingContext.getCurrentBindingsEntry();
+    }
+    
+    public Object executemethode(String mthod){
+        BindingContainer bds = getBindingContainer();
+        OperationBinding ob = bds.getOperationBinding(mthod);
+        
+        return ob.execute();
+    }
+
+    public String enregsitreProduitPfnl() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String supprimerProduitPfnl() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Delete");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String supprimePartieProduitPfnl() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Delete1");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+}
