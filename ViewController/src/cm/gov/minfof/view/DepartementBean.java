724b6f301d07a4f0cb620c36b8dfbf2e5f558207
abbo <tblaiso@gmail.com>
2016-08-17 15:06:56 +0100

    gitpfnl

---
+++ ViewController/src/cm/gov/minfof/view/DepartementBean.java
@@ -0,0 +1,60 @@
+package cm.gov.minfof.view;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class DepartementBean {
+    public DepartementBean() {
+    }
+
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+    
+    public BindingContainer getBindingContainer() {
+
+    BindingContext bindingContext = BindingContext.getCurrent();
+    return bindingContext.getCurrentBindingsEntry();
+    }
+    
+    public Object executemethode(String mthod){
+        BindingContainer bds = getBindingContainer();
+        OperationBinding ob = bds.getOperationBinding(mthod);
+        
+        return ob.execute();
+    }
+
+    public String enregistreDepartement() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String supprimeDepartement() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Delete");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String nouveauDept() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("CreateInsert");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+}
---
+++ ViewController/src/cm/gov/minfof/view/PermissionnaireBean.java
@@ -0,0 +1,58 @@
+package cm.gov.minfof.view;
+
+import oracle.adf.model.BindingContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class PermissionnaireBean {
+    public PermissionnaireBean() {
+    }
+
+    public BindingContainer getBindings() {
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+    
+    public BindingContainer getBindingContainer() {
+    BindingContext bindingContext = BindingContext.getCurrent();
+    return bindingContext.getCurrentBindingsEntry();
+    }
+    
+    public Object executemethode(String mthod){
+        BindingContainer bds = getBindingContainer();
+        OperationBinding ob = bds.getOperationBinding(mthod);
+        return ob.execute();
+    }
+
+    public String creerPermissionnaire() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("CreateInsert");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String enregistrerPermissionnaire() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Commit");
+        Object result = operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+
+    public String supprimerPermissionnaire() {
+        BindingContainer bindings = getBindings();
+        OperationBinding operationBinding = bindings.getOperationBinding("Delete");
+        Object result = operationBinding.execute();
+        executemethode("Commit");
+        if (!operationBinding.getErrors().isEmpty()) {
+            return null;
+        }
+        return null;
+    }
+}
